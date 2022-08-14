789162baa6e5e9d061dab0768f56e37b06864f3b
ManjimaVarghese <77264060+ManjimaVarghese@users.noreply.github.com>
2022-08-14 20:53:56 +0530

    Add files via upload

---
+++ aaa.sql
@@ -0,0 +1,30 @@
+/*
+SQLyog Community Edition- MySQL GUI
+MySQL - 5.1.32-community 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `registration` (
+	`id` double ,
+	`name` varchar (90),
+	`email` varchar (90),
+	`gender` varchar (90),
+	`date_of_birth` varchar (60),
+	`mob` double ,
+	`password` varchar (90),
+	`address` varchar (90),
+	`image` varchar (1500)
+); 
+insert into `registration` (`id`, `name`, `email`, `gender`, `date_of_birth`, `mob`, `password`, `address`, `image`) values('1','asa','asa@gmail.com','female','1990-12-22','998644','3333','sdfhj','hai');
+insert into `registration` (`id`, `name`, `email`, `gender`, `date_of_birth`, `mob`, `password`, `address`, `image`) values('2','aswanth','aswanth@gmail.com','female','2000-11-12','6643223678','2211','dfhjk,mnh','hai');
+insert into `registration` (`id`, `name`, `email`, `gender`, `date_of_birth`, `mob`, `password`, `address`, `image`) values('3','aswathi','aswathi@gmail.com','female','1997-12-22','45678900','3344','sdffgbbnmm','hai');
+insert into `registration` (`id`, `name`, `email`, `gender`, `date_of_birth`, `mob`, `password`, `address`, `image`) values('4','anu','anu@gmail.com','female','2333-04-03','2367899865533','6666','dfghjnbb','hai');
+insert into `registration` (`id`, `name`, `email`, `gender`, `date_of_birth`, `mob`, `password`, `address`, `image`) values('5','neha','neha@gmail.com','female','2018-04-07','1234567','123','ffghjjkkbshj','hai');
+insert into `registration` (`id`, `name`, `email`, `gender`, `date_of_birth`, `mob`, `password`, `address`, `image`) values('6','hana','hana@gmail.com','female','1993-04-04','98765433','7777','aswscgn dmk','hai');
+insert into `registration` (`id`, `name`, `email`, `gender`, `date_of_birth`, `mob`, `password`, `address`, `image`) values('7','ASA','asa@gmail.com','female','1990-12-22','345677889','11111','ASA HO MANIYUR PAYYOLI','hai');
+insert into `registration` (`id`, `name`, `email`, `gender`, `date_of_birth`, `mob`, `password`, `address`, `image`) values('8','HASNI','HASNI@GMAIL.COM','female','1994-04-03','987754333','5555','HASNI HYRTHJK','hai');
+insert into `registration` (`id`, `name`, `email`, `gender`, `date_of_birth`, `mob`, `password`, `address`, `image`) values('9','DEEPA','deepa@gnail.co','female','1995-05-04','988766548','77777','fghjenjufjnjj','hai');
+insert into `registration` (`id`, `name`, `email`, `gender`, `date_of_birth`, `mob`, `password`, `address`, `image`) values('10','asha','asagga@gmail.com','female','','9526455020','aswathi','dfghh','hai');
+insert into `registration` (`id`, `name`, `email`, `gender`, `date_of_birth`, `mob`, `password`, `address`, `image`) values('11','abhi','abhi@gmail.com','male','1995-02-22','9526455020','7777','ssdffghjk','hai');
+insert into `registration` (`id`, `name`, `email`, `gender`, `date_of_birth`, `mob`, `password`, `address`, `image`) values('12','MANU PRASAD .P K','manu@gmail.com','male','1889-02-03','9526455020','manu','KINARULLATHIL','hai');
